df69507ef84b675ff1c254139074c6ac1d0fd32f
fedi <fedi@atayen.us>
2022-02-17 15:52:16 +0100

    add swagger to index.js

--- index.js
+++ index.js
@@ -2,6 +2,27 @@
 import express from 'express'
 const app = express()
 import routes from './routes/index.js'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'API NODE' ,
+        customCss: '.swagger-ui .topbar { display: none }'
+    },
+      security: [{
+        bearerAuth: []
+      }]
+  };
+
+  const options = {
+    swaggerDefinition,
+    apis: [
+         './routes/*.js']
+    };
+    const swaggerSpec = swaggerJSDoc(options);
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(routes)
 
--- routes/content.js
+++ routes/content.js
@@ -5,6 +5,21 @@ import { getContent,getWebContent } from '../controllers/content.js'
 const route = Router()
 
 route.get('/contents', getContent)
-route.get('/test', getWebContent);
+/**
+ * @swagger
+ * /test:
+ *   get:
+ *     description: choosing todo number and returning the object of todo if it exist.
+ *     parameters:
+ *       - name: idTodo
+ *         in: query
+ *         type: string
+ *     responses:
+ *        "200":
+ *          description: an object with title and id
+ *        "500":
+ *          description: error:error message
+ */
+ route.get('/test', getWebContent);
 
 export default route
