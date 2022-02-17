56dd5d7596425776a90942b9a521319bf9933716
fedi <fedi@atayen.us>
2022-02-17 15:27:13 +0100

    add funtion to add data

--- models/content.js
+++ models/content.js
@@ -4,6 +4,10 @@ class ContentModel {
     findAll() {
         return db.getAll()
     }
+      //function to add data to content model in db
+      addData(data){
+        return db.set(data)
+    }
 }
 
 export const Content = new ContentModel()
