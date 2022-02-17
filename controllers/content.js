ca35a9657b358d512633e78158351401d9c3b8d4
fedi <fedi@atayen.us>
2022-02-17 15:37:03 +0100

    add function getwebcontent that return title and completed

--- controllers/content.js
+++ controllers/content.js
@@ -27,6 +27,21 @@ async function getDummyData() {
  * https://jsonplaceholder.typicode.com/todos/1
  * This method should only return title and completed
  */
-async function getWebContent() {
-    throw new Error('not implemented yet')
+ export const getWebContent= async (req,res) => {
+    try{
+       //fetching id of the todo by query parameter
+       let {idTodo} = req.query;
+       //if id is null or not a number return wrong id format
+        if(!idTodo || isNaN(+idTodo))return res.status(500).json({message:'wrong id format'});
+        //Do the query to fetch data from the external api;
+       let data = await rp({uri:`https://jsonplaceholder.typicode.com/todos/${idTodo}`, json: true }) 
+       //copy the received data and remove fields that are not mandatory by destructuring
+       let {id,userId,...todoInfo} = data;
+       //return object to user with 200 status code
+       res.status(200).json(todoInfo);
+    } catch(e){
+     res.status(500).json({message :'someting went wrong'});
+    }
+ 
+
 }
